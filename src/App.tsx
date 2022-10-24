9bb00afd0cbf255cde22bc9e897fd984a2b9bc59
Luigi <luigi.drnasin12@gmail.com>
2022-10-24 08:52:16 +0200

    finished project

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,27 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Navbar from "./components/Navbar";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Store from "./pages/Store";
+import { ShoppingCartProvider } from "./context/ShoppingCartContext";
+
 const App = () => {
-  return <div>app</div>;
+  return (
+    <>
+      <ShoppingCartProvider>
+        <Navbar />
+        <Container className="mb-4">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/store" element={<Store />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </Container>
+      </ShoppingCartProvider>
+    </>
+  );
 };
 
 export default App;
